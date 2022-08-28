5565b3d87d3ca40f57520c427d894b288d27c930
Andrey Mitko <andrey@crowdhaus.co.uk>
2022-08-28 15:36:04 +0300

    Added search bar

--- src/components/BooksContainer/index.jsx
+++ src/components/BooksContainer/index.jsx
@@ -4,7 +4,7 @@ import { Container, H2, BookList } from "./styles";
 
 import Book from "../Book";
 
-const BooksContainer = ({ books, pickBook, isPanelOpen }) => {
+const BooksContainer = ({ books, pickBook, isPanelOpen, title }) => {
   const [scroll, setScroll] = useState(0);
   const prevPanelState = useRef(false);
 
@@ -32,7 +32,7 @@ const BooksContainer = ({ books, pickBook, isPanelOpen }) => {
 
   return (
     <Container $isPanelOpen={isPanelOpen} $top={scroll}>
-      <H2>All books</H2>
+      <H2>{title}</H2>
       <BookList>
         {books && books.map((book) => <Book key={book.id} book={book} pickBook={pickBook} />)}
       </BookList>
